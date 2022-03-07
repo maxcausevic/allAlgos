e1e3ea42ce8d113cf413d5d05685bd7ecddbcd48
Maxine Causevic <maxcausevic@gmail.com>
2022-03-07 11:54:08 -0600

    palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,6 @@
 var isPalindrome = function(x) {
-    ;
-}
+    let reversed = x.toString().split('').reverse().join('')
+    return (x.toString() === reversed)
+};
+
+console.log(isPalindrome(123))
